b2fa9eeaaa3eee6731c2842359aeff5e6d492411
Anton Shulgin <anton.shulgin@gmx.com>
2017-01-11 21:02:42 +0300

    added Util.pm; moved generic stuff to Util.pm

---
+++ Util.pm
@@ -0,0 +1,40 @@
+package Util;
+
+use strict;
+use warnings;
+
+sub format_results {
+	my @results = @_;
+	my @output = ();
+	my $title;
+	my $url;
+	if (scalar(@results) <= 0) {
+		return "Nothing found.";
+	}
+	for (my $idx = 0; $idx < scalar(@results); $idx += 1) {
+		$title = $results[$idx]->{title};
+		$url = produce_url($results[$idx]->{path});
+		push(@output, "[$idx] $title: $url");
+	}
+	return join(' :: ', @output);
+}
+
+sub produce_url {
+	my $url = shift;
+	my $prefix = shift;
+	return defined($prefix) ? "$prefix/$url" : $url;
+}
+
+sub format_term {
+	my $term = shift;
+	$term =~ s/[^0-9a-z ]//gi;
+	return $term;
+}
+
+sub trim_whitespaces {
+	my $content = shift;
+	$content =~ s/^\s+|\r|\n|\s+$//g;
+	return $content;
+}
+
+1;
